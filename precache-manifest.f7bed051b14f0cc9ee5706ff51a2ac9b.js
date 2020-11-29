a95cb3c2f545facb311adcce0212e27311557425
idevnet <newneodev20@outlook.com>
2020-11-29 20:16:42 +0100

    Updates

---
+++ precache-manifest.f7bed051b14f0cc9ee5706ff51a2ac9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e852f2e29497aceb83378ff1597bda",
+    "url": "/to-dos/index.html"
+  },
+  {
+    "revision": "ef7788fe6bde62c818ce",
+    "url": "/to-dos/static/css/main.29b540cd.chunk.css"
+  },
+  {
+    "revision": "7be0e9b51603606379c0",
+    "url": "/to-dos/static/js/2.6d2a2195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-dos/static/js/2.6d2a2195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7788fe6bde62c818ce",
+    "url": "/to-dos/static/js/main.2d8f8c87.chunk.js"
+  },
+  {
+    "revision": "6e36f3f9114add7d485c",
+    "url": "/to-dos/static/js/runtime-main.98ca1cae.js"
+  }
+]);
